0ea15e9b3103a477aa7fb598a25a98f5b0c7f77a
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-12-12 17:59:45 +0100

    still testing with getnextline

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,44 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:21:42 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/10 13:23:46 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:58:19 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "get_next_line.h"
+#include <fcntl.h> 
 
-char	get_next_line(int fd)
+typedef struct s_fd_buffer
 {
-	char buffer[BUFFER_SIZE];
+	char	**buffers;
+	int		*fds;
+}t_buffer;
+char	*get_next_line(int fd)
+{
+	size_t bsize;
+	size_t	scanned;
+	char	*s;
+	int		i;
+	static t_buffer buffer;
 
-	buffer[0] = 'c';
-	buffer[1] = fd;
-	return (buffer[0]);
+	buffer.buffers = (char **) malloc( 5* sizeof(char *));
+	buffer.buffers[0] = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	bsize = BUFFER_SIZE;
+	scanned = read(fd, buffer.buffers[0], bsize);
+	s = (char *) malloc((scanned + 1) * sizeof(char));
+	i = 0;
+	while (i < scanned)
+	{
+		s[i] = buffer.buffers[0][i];
+		i++;
+	}
+	s[i] = 0;
+	return (s);
 }
 
 int main()
 {
-	get_next_line(2);
-	printf("%c", get_next_line(2));
+	int fd = open("lol", O_RDONLY);
+
+	printf("%s\n", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:24:17 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/10 13:29:36 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:18:24 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
+# include <stdlib.h>
 # include <fcntl.h>
-char	*
+char	*get_next_line(int fd);
 #endif

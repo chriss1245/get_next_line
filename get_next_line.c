c3ef81d9a9ef9b4e8d5ed8b096fa0a43012129b9
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-12-18 14:55:37 +0100

    passed norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,14 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:21:42 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/18 13:38:06 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/18 14:55:18 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
-#include <stdio.h>
 
-char	*update_str(char *s, char *buffer, int offset)
+static char	*update_str(char *s, char *buffer, int offset)
 {
 	char	*ns;
 
@@ -25,75 +24,54 @@ char	*update_str(char *s, char *buffer, int offset)
 	return (ns);
 }
 
+static char	*get_next_line_scan_error(char *s, int buffer_len)
+{
+	if (buffer_len > 0)
+		return (s);
+	free(s);
+	return (0);
+}
+
+static char	*get_next_line_scan_finished(char *s, char *buffer, int scanned)
+{
+	int	idx;
+
+	idx = ft_strnchr_idx(buffer, '\n');
+	if (idx < 0)
+	{
+		s = update_str(s, buffer, scanned);
+		ft_bzero(buffer, BUFFER_SIZE);
+		return (s);
+	}
+	idx = ft_strnchr_idx(buffer, '\n');
+	return (update_str(s, buffer, idx + 1));
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
-	int		scanned;
+	int			scanned;
 	char		*s;
-	char		*ns;
 	int			aux;
 	int			buffer_len;
-	
+
 	buffer_len = ft_strlen(buffer);
 	aux = ft_strnchr_idx(buffer, '\n');
-	s = (char *) malloc(1*sizeof(char));
-	*s = 0;
- 	if (aux > -1)
+	s = ft_strnjoin("", "", 0, 0);
+	if (aux > -1)
 		return (update_str(s, buffer, aux + 1));
-	s = update_str(s, buffer, ft_strlen(buffer));
-	printf("s (%s)", s);
+	s = update_str(s, buffer, buffer_len);
 	scanned = read(fd, buffer, BUFFER_SIZE);
 	aux = ft_strnchr_idx(buffer, '\n');
 	if (scanned <= 0)
-	{
-		if (buffer_len > 0 && aux < 0)
-			return (s);
-		free(s);
-		return (0);
-	}
+		return (get_next_line_scan_error(s, buffer_len));
 	while (aux < 0 && scanned == BUFFER_SIZE)
 	{
-		ns = ft_strnjoin(s, buffer, ft_strlen(s), BUFFER_SIZE);
-		free(s);
-		s = ns;
+		s = update_str(s, buffer, BUFFER_SIZE);
 		scanned = read(fd, buffer, BUFFER_SIZE);
 		aux = ft_strnchr_idx(buffer, '\n');
 	}
 	if (scanned < BUFFER_SIZE && scanned >= 0)
-	{
-		if (aux < 0)
-		{
-			ns = ft_strnjoin(s, buffer, ft_strlen(s), scanned);
-			ft_bzero(buffer, BUFFER_SIZE);
-			free(s);
-			return(ns);
-		}
-		else
-		{
-			ft_bzero(buffer + scanned, BUFFER_SIZE - scanned);
-			aux = ft_strnchr_idx(buffer, '\n');
-		}
-	}
-	ns = ft_strnjoin(s, buffer, ft_strlen(s), (size_t) aux + 1);
-	free(s);
-	ft_memcpy(buffer, buffer + aux + 1, BUFFER_SIZE - aux - 1);
-	ft_bzero(buffer + BUFFER_SIZE - aux -1, aux + 1);
-	return (ns);
+		return (get_next_line_scan_finished(s, buffer, scanned));
+	return (update_str(s, buffer, aux + 1));
 }
-
-/*
-int main()
-{
-	int fd = open("lol", O_RDONLY);
-
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	
-	close(fd);
-}*/

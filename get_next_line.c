0e991be4f1b239d698ce93ed16fdc6d93081863f
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-12-18 13:38:19 +0100

    cleaning the code

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,24 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:21:42 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/18 11:07:19 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/18 13:38:06 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+
+char	*update_str(char *s, char *buffer, int offset)
+{
+	char	*ns;
+
+	ns = ft_strnjoin(s, buffer, ft_strlen(s), offset);
+	free(s);
+	ft_memcpy(buffer, buffer + offset, BUFFER_SIZE - offset);
+	ft_bzero(buffer + BUFFER_SIZE - offset, offset);
+	return (ns);
+}
 
 char	*get_next_line(int fd)
 {
@@ -25,27 +36,20 @@ char	*get_next_line(int fd)
 	
 	buffer_len = ft_strlen(buffer);
 	aux = ft_strnchr_idx(buffer, '\n');
+	s = (char *) malloc(1*sizeof(char));
+	*s = 0;
  	if (aux > -1)
-	{
-		s = ft_strnjoin(buffer, "", ft_strnchr_idx(buffer, '\n') + 1, 0);
-		ft_memcpy(buffer, buffer + aux + 1, BUFFER_SIZE - aux -1);
-		ft_bzero(buffer + BUFFER_SIZE - aux - 1, aux + 1);
-
-		return (s);
-	}
-	s = ft_strnjoin(buffer, "", ft_strnchr_idx(buffer, '\0') , 0);
+		return (update_str(s, buffer, aux + 1));
+	s = update_str(s, buffer, ft_strlen(buffer));
+	printf("s (%s)", s);
 	scanned = read(fd, buffer, BUFFER_SIZE);
 	aux = ft_strnchr_idx(buffer, '\n');
 	if (scanned <= 0)
 	{
-		free(s);
-		ns = 0;
 		if (buffer_len > 0 && aux < 0)
-		{
-			ns = ft_strnjoin(buffer, "", ft_strlen(buffer), 0);
-			ft_bzero(buffer, BUFFER_SIZE);
-		}
-		return (ns);
+			return (s);
+		free(s);
+		return (0);
 	}
 	while (aux < 0 && scanned == BUFFER_SIZE)
 	{
@@ -55,18 +59,20 @@ char	*get_next_line(int fd)
 		scanned = read(fd, buffer, BUFFER_SIZE);
 		aux = ft_strnchr_idx(buffer, '\n');
 	}
-	if (aux < 0 && scanned < BUFFER_SIZE && scanned >= 0)
+	if (scanned < BUFFER_SIZE && scanned >= 0)
 	{
-		ft_bzero(buffer + scanned, BUFFER_SIZE - scanned);
-		ns = ft_strnjoin(s, buffer, ft_strlen(s), scanned);
-		ft_bzero(buffer, BUFFER_SIZE);
-		free(s);
-		return(ns);
-	}
-	else if (aux >= 0 && scanned >= 0 && scanned < BUFFER_SIZE)
-	{
-		ft_bzero(buffer + scanned, BUFFER_SIZE - scanned);
-		aux = ft_strnchr_idx(buffer, '\n');
+		if (aux < 0)
+		{
+			ns = ft_strnjoin(s, buffer, ft_strlen(s), scanned);
+			ft_bzero(buffer, BUFFER_SIZE);
+			free(s);
+			return(ns);
+		}
+		else
+		{
+			ft_bzero(buffer + scanned, BUFFER_SIZE - scanned);
+			aux = ft_strnchr_idx(buffer, '\n');
+		}
 	}
 	ns = ft_strnjoin(s, buffer, ft_strlen(s), (size_t) aux + 1);
 	free(s);
@@ -74,6 +80,7 @@ char	*get_next_line(int fd)
 	ft_bzero(buffer + BUFFER_SIZE - aux -1, aux + 1);
 	return (ns);
 }
+
 /*
 int main()
 {
